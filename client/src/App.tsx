681ef966bd77007a8b3fab9ce33dad18211b9691
yanpatla <tomipatlayan@gmail.com>
2022-09-28 17:34:19 +0300

    FM-0001:feat : Setting the client side
    Adding all the dependencies and configuration for development and building of the application

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./a.scss";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="pp">
+       
+        hola
+      
+    </div>
+  );
+}
+
+export default App;
